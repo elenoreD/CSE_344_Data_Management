fae7671ddb7196400c497ed3c5d7aed2a21bbbe8
elenoreD <elenore@umich.edu>
2020-06-14 18:31:09 -0400

    change to local database

--- hw5/createTables.sql
+++ hw5/createTables.sql
@@ -1,14 +1,19 @@
-create EXTERNAL table USERS (username varchar(20),
+create table USERS (username varchar(20),
                     password varbinary(20),
                     salt varbinary(20),
-                    balance int) WITH (DATA_SOURCE = CSE344_EXTERNAL);
+                    balance int, 
+                    PRIMARY KEY (username));
 
-create EXTERNAL table RESERVATIONS (reservation_id int not null,
+create table RESERVATIONS (reservation_id int IDENTITY(1,1) not null,
                         user_name varchar(20),
                         flight_id1 int,
                         flight_id2 int,
                         isPaid bit,
-                        isCancelled bit) WITH (DATA_SOURCE = CSE344_EXTERNAL);
+                        isCancelled bit,
+                        PRIMARY KEY (reservation_id),
+                        FOREIGN KEY (user_name) REFERENCES USERS(username) ON DELETE CASCADE,
+                        FOREIGN KEY (flight_id1) REFERENCES FLIGHTS(fid) ON DELETE CASCADE,
+                        FOREIGN KEY (flight_id2) REFERENCES FLIGHTS(fid) ON DELETE CASCADE);
 
 CREATE INDEX F_origin ON FLIGHTS(origin_city);
 CREATE INDEX F_dest ON FLIGHTS(dest_city);
