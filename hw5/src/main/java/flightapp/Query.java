b5cd6b458f47520629715d37d2c49410685c6417
elenoreD <elenore@umich.edu>
2020-06-19 13:30:37 -0400

    search function

--- hw5/src/main/java/flightapp/Query.java
+++ hw5/src/main/java/flightapp/Query.java
@@ -16,6 +16,7 @@ public class Query {
   private Connection conn;
   private boolean isLogin;
   private String currentUser;
+  private List<List<Integer>> itineraries;
 
   // Password hashing parameter constants
   private static final int HASH_STRENGTH = 65536;
@@ -40,11 +41,12 @@ public class Query {
   private PreparedStatement locateUserStatement;
 
   // For search for direct flight
-  private static final String directSearch_SQL = "SELECT TOP ( ? ) day_of_month,carrier_id,flight_num,origin_city,dest_city,actual_time,capacity,price FROM Flights WHERE origin_city = ?  AND dest_city =  ? AND day_of_month = ?  ORDER BY actual_time ASC";
+  private static final String directSearch_SQL = "SELECT TOP ( ? ) fid, day_of_month,carrier_id,flight_num,origin_city,dest_city,actual_time,capacity,price FROM Flights WHERE origin_city = ?  AND dest_city =  ? AND day_of_month = ?  ORDER BY actual_time ASC";
   private PreparedStatement directSearchStatement;
 
   // For search for direct & one stop flight
-  private static final String multipleSearch_SQL = "SELECT TOP ( ? ) F1.day_of_month AS day1, F1.carrier_id as carrier1,F1.flight_num as flightnum1,F1.origin_city as origincity1,F1.dest_city as destcity1,F1.actual_time as actualtime1,F1.capacity as capacity1,F1.price as price1 FROM Flights F1, Flights F2 WHERE origin_city = ?  AND dest_city =  ? AND day_of_month = ?  ORDER BY actual_time ASC";
+  private static final String multipleSearch_SQL = "SELECT * FROM (SELECT  TOP ( ? ) * from (SELECT 2 AS NUMBER, (F1.actual_time + F2.actual_time) as total_time, F1.fid as fid1, F1.day_of_month AS day1, F1.carrier_id as carrier1,F1.flight_num as flightnum1,F1.origin_city as origincity1,F1.dest_city as destcity1,F1.actual_time as actualtime1,F1.capacity as capacity1,F1.price as price1, F2.fid as fid2, F2.day_of_month AS day2, F2.carrier_id as carrier2,F2.flight_num as flightnum2,F2.origin_city as origincity2,F2.dest_city as destcity2,F2.actual_time as actualtime2,F2.capacity as capacity2,F2.price as price2 FROM Flights F1, Flights F2 WHERE F1.dest_city = F2.origin_city and F1.origin_city = ?  AND F2.dest_city =  ? AND F1.day_of_month = ? and F2.day_of_month = ?  and F1.month_id=F2.month_id and F1.canceled=0 and F2.canceled=0                UNION        SELECT 1 AS NUMBER, F3.actual_time as total_time, F3.fid as fid1, F3.day_of_month AS day1, F3.carrier_id as carrier1,F3.flight_num as flightnum1,F3.origin_city as origincity1,F3.dest_city as destcity1,F3.actual_time as actualtime1,F3.capacity as capacity1,F3.price as price1,  NULL as fid2, NULL AS day2, NULL as carrier2, NULL as flightnum2 , NULL as origincity2,NULL as destcity2,NULL as actualtime2,NULL as capacity2,NULL as price2 FROM Flights F3 WHERE F3.origin_city = ?  AND F3.dest_city =  ? AND F3.day_of_month = ?  ) AS TOTAL_FLIGHT ORDER BY NUMBER, total_time) AS M ORDER BY total_time ";
+
   private PreparedStatement multipleSearchStatement;
 
   // TODO: YOUR CODE HERE
@@ -313,15 +315,16 @@ public class Query {
 
         StringBuffer sb = new StringBuffer();
         try {
-          unsafeSearchStatement.setInt(1, numberOfItineraries);
-          unsafeSearchStatement.setString(2, originCity);
-          unsafeSearchStatement.setString(3, destinationCity);
-          unsafeSearchStatement.setInt(4, dayOfMonth);
-          unsafeSearchStatement.executeUpdate();
-          unsafeSearchStatement.close();
-          ResultSet oneHopResults = unsafeSearchStatement.executeQuery();
-
+          directSearchStatement.setInt(1, numberOfItineraries);
+          directSearchStatement.setString(2, originCity);
+          directSearchStatement.setString(3, destinationCity);
+          directSearchStatement.setInt(4, dayOfMonth);
+    
+          ResultSet oneHopResults = directSearchStatement.executeQuery();
+          itineraries = new ArrayList<>();
+          int i = 0;
           while (oneHopResults.next()) {
+            int result_fid = oneHopResults.getInt("fid");
             int result_dayOfMonth = oneHopResults.getInt("day_of_month");
             String result_carrierId = oneHopResults.getString("carrier_id");
             String result_flightNum = oneHopResults.getString("flight_num");
@@ -331,10 +334,15 @@ public class Query {
             int result_capacity = oneHopResults.getInt("capacity");
             int result_price = oneHopResults.getInt("price");
 
-            sb.append("Day: " + result_dayOfMonth + " Carrier: " + result_carrierId + " Number: " + result_flightNum
-                + " Origin: " + result_originCity + " Destination: " + result_destCity + " Duration: " + result_time
+            itineraries.add(List.of(result_dayOfMonth, result_fid, result_capacity));
+            
+            sb.append("Itinerary " + (i++) + ": " + "1 flight(s), " + result_time + " minutes" + "\n" + "ID: " + result_fid + " Day: " + result_dayOfMonth + " Carrier: " + result_carrierId + " Number: " + result_flightNum
+                + " Origin: " + result_originCity + " Dest: " + result_destCity + " Duration: " + result_time
                 + " Capacity: " + result_capacity + " Price: " + result_price + "\n");
+            
+
           } 
+
           oneHopResults.close();
         } catch (SQLException e) {
           e.printStackTrace();
@@ -351,33 +359,65 @@ public class Query {
         
 
         try {
-          // one hop itineraries
-          directSearchStatement.setInt(1, numberOfItineraries);
-          directSearchStatement.setString(2, originCity);
-          directSearchStatement.setString(3, destinationCity);
-          directSearchStatement.setInt(4, dayOfMonth);
-          directSearchStatement.executeUpdate();
-          directSearchStatement.close();
-          ResultSet oneHopResults = directSearchStatement.executeQuery();
+          // one stop itineraries
+          multipleSearchStatement.setInt(1, numberOfItineraries);
+          multipleSearchStatement.setString(2, originCity);
+          multipleSearchStatement.setString(3, destinationCity);
+          multipleSearchStatement.setInt(4, dayOfMonth);
+          multipleSearchStatement.setInt(5, dayOfMonth);
+          multipleSearchStatement.setString(6, originCity);
+          multipleSearchStatement.setString(7, destinationCity);
+          multipleSearchStatement.setInt(8, dayOfMonth);
+  
+          ResultSet multiResults = multipleSearchStatement.executeQuery();
+          itineraries = new ArrayList<>();
+          int i = 0;
+          while (multiResults.next()) {
+            int result_fid = multiResults.getInt("fid1");
+            int result_num = multiResults.getInt("NUMBER");
+            int result_dayOfMonth = multiResults.getInt("day1");
+            String result_carrierId = multiResults.getString("carrier1");
+            String result_flightNum = multiResults.getString("flightnum1");
+            String result_originCity = multiResults.getString("origincity1");
+            String result_destCity = multiResults.getString("destcity1");
+            int result_time = multiResults.getInt("actualtime1");
+            int result_total_time = multiResults.getInt("total_time");
+            int result_capacity = multiResults.getInt("capacity1");
+            int result_price = multiResults.getInt("price1");
 
-          while (oneHopResults.next()) {
-            int result_dayOfMonth = oneHopResults.getInt("day_of_month");
-            String result_carrierId = oneHopResults.getString("carrier_id");
-            String result_flightNum = oneHopResults.getString("flight_num");
-            String result_originCity = oneHopResults.getString("origin_city");
-            String result_destCity = oneHopResults.getString("dest_city");
-            int result_time = oneHopResults.getInt("actual_time");
-            int result_capacity = oneHopResults.getInt("capacity");
-            int result_price = oneHopResults.getInt("price");
+            itineraries.add(List.of(result_dayOfMonth, result_fid, result_capacity));
 
-            sb.append("Day: " + result_dayOfMonth + " Carrier: " + result_carrierId + " Number: " + result_flightNum
-                + " Origin: " + result_originCity + " Destination: " + result_destCity + " Duration: " + result_time
+            sb.append("Itinerary " + (i++) + ": " + result_num + " flight(s), " + result_total_time + " minutes" + "\n" + "ID: " + result_fid  + " Day: " + result_dayOfMonth + " Carrier: " + result_carrierId + " Number: " + result_flightNum
+                + " Origin: " + result_originCity + " Dest: " + result_destCity + " Duration: " + result_time
                 + " Capacity: " + result_capacity + " Price: " + result_price + "\n");
+
+            if (result_num==2) {
+            int result_fid1 = result_fid;
+            int result_capacity1 = result_capacity;
+            result_fid = multiResults.getInt("fid2");
+            result_dayOfMonth = multiResults.getInt("day2");
+            result_carrierId = multiResults.getString("carrier2");
+            result_flightNum = multiResults.getString("flightnum2");
+            result_originCity = multiResults.getString("origincity2");
+            result_destCity = multiResults.getString("destcity2");
+            result_time = multiResults.getInt("actualtime2");
+            result_capacity = multiResults.getInt("capacity2");
+            result_price = multiResults.getInt("price2");
+
+            itineraries.set(i-1, List.of(result_dayOfMonth, result_fid1, result_capacity1, result_fid, result_capacity));
+
+            sb.append("ID: " + result_fid  + " Day: " + result_dayOfMonth + " Carrier: " + result_carrierId + " Number: " + result_flightNum
+                + " Origin: " + result_originCity + " Dest: " + result_destCity + " Duration: " + result_time
+                + " Capacity: " + result_capacity + " Price: " + result_price + "\n");
+            }
           } 
-          oneHopResults.close();
+          multiResults.close();
         } catch (SQLException e) {
           e.printStackTrace();
         }
+
+        
+
         if (sb.length() == 0){
           return "No flights match your selection\n";
         } else {
